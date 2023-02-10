7f4153fbcd24619c966ce17a18a1dcd6d9ed7cf9
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-02-10 15:02:06 +0100

    Détection des ensembles de points au-dessus d'un certain seuil

--- algo_detection_contours/src/main.cpp
+++ algo_detection_contours/src/main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/imgproc.hpp"
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -8,6 +9,9 @@ int main(int argc, char** argv)
 	// On charge l'image donnée en paramètre (par défaut img.png dans le dossier source) en noir et blanc
 	cv::CommandLineParser parser(argc, argv, "{@image | ../../../img.png | Image sur laquelle on va opérer nos transformations}");
 	cv::Mat image_source = imread(parser.get<cv::String>( "@image" ), cv::IMREAD_GRAYSCALE);
+
+	// Première étape: séparation des pixels selon différent seuils:
+	cv::threshold(image_source, image_source, 128, 255, cv::THRESH_BINARY);
 
 	// On affiche l'image
 	cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
