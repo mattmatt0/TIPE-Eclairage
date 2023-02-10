30539ac7312e3d8c2be73d5157f103968e62ea90
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-02-10 14:48:48 +0100

    Nettoyage du code

--- algo_detection_contours/src/main.cpp
+++ algo_detection_contours/src/main.cpp
@@ -3,14 +3,17 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-using namespace cv;
-
 int main(int argc, char** argv)
 {
-	CommandLineParser parser(argc, argv, "{@image | ../../../img.png | Image sur laquelle on va opérer nos transformations}");
-	Mat image_source = imread(samples::findFile(parser.get<String>( "@image" )),IMREAD_COLOR);
-	namedWindow("image", WINDOW_AUTOSIZE);
-	imshow("image", image_source);
-	while(waitKeyEx() != 113);
+	// On charge l'image donnée en paramètre (par défaut img.png dans le dossier source) en noir et blanc
+	cv::CommandLineParser parser(argc, argv, "{@image | ../../../img.png | Image sur laquelle on va opérer nos transformations}");
+	cv::Mat image_source = imread(parser.get<cv::String>( "@image" ), cv::IMREAD_GRAYSCALE);
+
+	// On affiche l'image
+	cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
+	cv::imshow("image", image_source);
+	
+	// On attend que la touche `q` ait été pressée
+	while(cv::waitKeyEx() != 113);
 	return 0;
 }
