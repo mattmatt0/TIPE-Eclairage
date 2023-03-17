2bf7b8070ccd3cf268e6c1811a421eb38d02a4ea
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-03-17 15:33:58 +0100

    Réorganisation

--- tests/detection_contours/src/main.cpp
+++ tests/detection_contours/src/main.cpp
@@ -1,14 +1,10 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include "opencv2/imgproc.hpp"
+#include "detection_contours/detection_contours.hpp"
 #include <iostream>
-
 
 int main(int argc, char** argv)
 {
 	// On charge l'image donnée en paramètre (par défaut img.png dans le dossier source) en noir et blanc
-	cv::CommandLineParser parser(argc, argv, "{@image | ../../../images/toy_example.jpg | Image sur laquelle on va opérer nos transformations}");
+	cv::CommandLineParser parser(argc, argv, "{@image | ../../../../images/toy_example.jpg | Image sur laquelle on va opérer nos transformations}");
 	cv::Mat image_chargee = imread(parser.get<cv::String>( "@image" ), cv::IMREAD_GRAYSCALE);
 
 	// On floute un peu l'image pour réduire le bruit
@@ -31,36 +27,7 @@ int main(int argc, char** argv)
 	// Calcul des contours
 	for(int i = 0; i < NB_SEUILS; ++i)
 	{
-		// Calcul des dérivées
-		cv::Mat gradient_x, gradient_y;
-		Sobel(ensembles_X[i], gradient_x, CV_32F, 1, 0, 3);
-		Sobel(ensembles_X[i], gradient_y, CV_32F, 0, 1, 3);
-
-		// Conversion en flottants parce qu'il le faut
-		cv::normalize(gradient_x, gradient_x);
-		cv::normalize(gradient_y, gradient_y);
-		gradient_x *= 256.0;
-		gradient_y *= 256.0;
-
-		// Conversion (x,y) -> (norme, direction)
-		cv::Mat direction;
-		cv::Mat norme;
-		cv::phase(gradient_x, gradient_y, direction, true); // "true" pour avoir des degrés	
-		cv::magnitude(gradient_x, gradient_y, norme);
-
-		// Décision des pixels qui font partie d'un contour ou non
-		cv::Mat contour;
-		cv::threshold(norme, contour, 0.5, 1.0, cv::THRESH_BINARY);
-
-		// Ajout de l'orientation dans une matrice à trois canaux 
-		// (le 1er représentant l'orientation, le 2e valant 1.0 tout le temps 
-		//     et le 3e l'appartenance à un contour)
-		std::vector<cv::Mat> canaux;
-		canaux.push_back(direction);
-		canaux.push_back(cv::Mat(direction.size(), CV_32F, 1.0));
-		canaux.push_back(contour);
-		cv::Mat gradient;
-		cv::merge(canaux,contours[i]);
+		calcule_contours(ensembles_X[i],contours[i]);		
 	}
 
 	// Calcul de S et O
