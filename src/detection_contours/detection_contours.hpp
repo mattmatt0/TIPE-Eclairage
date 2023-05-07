92cd0d3abfcd599447fd2f3427d5fc5916d06d68
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-07 18:32:44 +0200

    Ajustements sur la fonction type2str

--- src/detection_contours/detection_contours.hpp
+++ src/detection_contours/detection_contours.hpp
@@ -7,27 +7,29 @@
 using namespace cv;
 using namespace std;
 
-string type2str(int type) {
-  string r;
+string type2str(int type) 
+{
+	string r;
 
-  uchar depth = type & CV_MAT_DEPTH_MASK;
-  uchar chans = 1 + (type >> CV_CN_SHIFT);
+	uchar profondeur = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
 
-  switch ( depth ) {
-    case CV_8U:  r = "8U"; break;
-    case CV_8S:  r = "8S"; break;
-    case CV_16U: r = "16U"; break;
-    case CV_16S: r = "16S"; break;
-    case CV_32S: r = "32S"; break;
-    case CV_32F: r = "32F"; break;
-    case CV_64F: r = "64F"; break;
-    default:     r = "User"; break;
-  }
+	switch(profondeur) 
+	{
+		case CV_8U:  r = "8U"; break;
+		case CV_8S:  r = "8S"; break;
+		case CV_16U: r = "16U"; break;
+		case CV_16S: r = "16S"; break;
+		case CV_32S: r = "32S"; break;
+		case CV_32F: r = "32F"; break;
+		case CV_64F: r = "64F"; break;
+		default:     r = "User"; break;
+	}
 
-  r += "C";
-  r += (chans+'0');
+	r += "C";
+	r += (chans+'0');
 
-  return r;
+	return r;
 }
 
 struct TroisCanaux
