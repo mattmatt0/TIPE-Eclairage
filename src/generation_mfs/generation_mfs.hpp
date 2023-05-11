484ad012c7be3ef0690847200b3965ba47c28636
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-11 20:28:36 +0200

    Chargement d'images, début de création de R (incomplet)

--- src/generation_mfs/generation_mfs.hpp
+++ src/generation_mfs/generation_mfs.hpp
@@ -5,13 +5,53 @@ vector<Mat> charge_images(string repertoire, string extension)
 	// Charge toutes les images dans `repertoire` avec l'extension `extension`
 	// `repertoire` ne doit pas comporter de "/" à la fin.
 	// `extension` ne doit pas comporter de "." au début
+	vector<cv::String> emplacements;
+	glob("/*." + extension, emplacements, false);
+
+	vector<Mat> images;
+	size_t nb_images = fn.size(); 
+	Mat image_courante;
+
+	for(size_t i = 0; i < nb_images; ++i)
+	{
+		image_courante = imread(emplacements[i]);
+		bilateralFilter(image_courante, image_courante, 10, 20, 5);
+    	images.push_back(image_courante);
+	}
+	
+	return images;
 }
 
-Mat calcule_R(vector<Mat> images, int t, int T, int nb_orientations)
+Mat calcule_R(vector<Mat> ensembles_O, int t, int T, int nb_orientations)
 {
 	// Calcule R_t à partir du vecteur des valeurs de O à chaque instant t.
 	// R_t est l'ensemble des points ayant une orientation fixe pendant T images.
 	// L'image renvoyée contient des entiers valant 0 si le pixel n'est pas dans R, et un entier représentant l'orientation du pixel sinon.
+	int seuil = 360 / (nb_orientations+1);
+	int taille_x = ensembles_O.at(0).size().height;
+	int taille_y = ensembles_O.at(0).size().width;
+	// Création de la première image à laquelle on va comparer les images suivantes.
+	// On va arrondir les valeurs des orientations comme il faut, dans un demi-tour
+	Mat res = Mat(ensembles_O.at(0).size(), CV_8UC1)
+	for(int x = 0; x < taille_x; ++x)
+	{
+		for(int y = 0; y < taille_y; ++y)
+		{
+			res.at<int>(x,y) 
+		}
+	}
+
+	// Puis on vérifie la stabilité des orientations
+	for(int x = 0; x < taille_x; ++x)
+	{
+		for(int y = 0; y < taille_y; ++y)
+		{
+			for(int i = t+1; i < t+T; ++i)
+			{
+
+			}
+		}
+	}
 }
 
 Mat calcule_D(vector<Mat> images, int t,  int nb_orientations)
