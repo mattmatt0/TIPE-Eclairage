0fcd289b3779dfbad865412b74257de2d82097fa
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-13 08:26:41 +0200

    Correction du chargement des images dans un répertoire

--- src/generation_mfs/generation_mfs.hpp
+++ src/generation_mfs/generation_mfs.hpp
@@ -6,14 +6,16 @@ vector<Mat> charge_images(string repertoire, string extension)
 	// `repertoire` ne doit pas comporter de "/" à la fin.
 	// `extension` ne doit pas comporter de "." au début
 	vector<cv::String> emplacements;
-	glob("/*." + extension, emplacements, false);
+	cout << "Recherche d'images dans " << repertoire + "/*." + extension << endl;
+	glob(repertoire + "/*." + extension, emplacements, false);
 
 	vector<Mat> images;
-	size_t nb_images = fn.size(); 
+	size_t nb_images = emplacements.size(); 
 	Mat image_courante;
 
 	for(size_t i = 0; i < nb_images; ++i)
 	{
+		cout << "Lecture de " + emplacements[i] << endl;
 		image_courante = imread(emplacements[i], IMREAD_GRAYSCALE);
 		bilateralFilter(image_courante, image_courante, 10, 20, 5);
     	images.push_back(image_courante);
@@ -32,7 +34,7 @@ Mat calcule_R(vector<Mat> ensembles_O, int t, int T, int nb_orientations)
 	int taille_y = ensembles_O.at(0).size().width;
 	// Création de la première image à laquelle on va comparer les images suivantes.
 	// On va arrondir les valeurs des orientations comme il faut, dans un demi-tour
-	Mat res = Mat(ensembles_O.at(0).size(), CV_8UC1)
+	Mat res = Mat(ensembles_O.at(0).size(), CV_8UC1);
 	for(int x = 0; x < taille_x; ++x)
 	{
 		for(int y = 0; y < taille_y; ++y)
@@ -60,6 +62,7 @@ Mat calcule_R(vector<Mat> ensembles_O, int t, int T, int nb_orientations)
 	return res;
 }
 
+/*
 Mat calcule_D(vector<Mat> images, int t,  int nb_orientations)
 {
 	// Calcule l'ensemble D_t des pixels pour lesquels un mouvement a été detecté à l'instant t
@@ -67,4 +70,4 @@ Mat calcule_D(vector<Mat> images, int t,  int nb_orientations)
 	// Si le pixel vaut 0, aucun mouvement n'a été détecté.
 	// Sinon, le pixel vaut 1
 }
-
+*/
--- tests/generation_mfs/src/main.cpp
+++ tests/generation_mfs/src/main.cpp
@@ -10,14 +10,14 @@ int main(int argc, char** argv)
 	for(int i = 0; i < images.size(); ++i)
 	{
 		vector<Mat> ensembles_X = separe_en_seuils(images.at(i), NB_SEUILS);
-		vector<Mat> contours = calcul_contours(images.at(i), ensembles_X);
+		vector<Mat> contours = calcul_contours(images.at(i), ensembles_X, NB_SEUILS);
 		vector<Mat> S_et_O = calcul_S_et_O(contours, NB_SEUILS);
 		ensembles_O.push_back(S_et_O.at(0));
 	}
 	for(int t = 0; t + T < ensembles_O.size(); ++t)
 	{
 		Mat res = calcule_R(ensembles_O, t, T, NB_ORIENTATIONS);
-		imgshow("R(" + to_string(t) + ")", res);
+		imshow("R(" + to_string(t) + ")", res);
 		while(waitKeyEx() != 113);
 	}
 	return 0;
