b7dc5404838ae7887c6c592db19208e6e503cc51
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-13 09:40:55 +0200

    Déplacement de certains outils

--- src/detection_contours/detection_contours.hpp
+++ src/detection_contours/detection_contours.hpp
@@ -1,36 +1,4 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include "opencv2/imgproc.hpp"
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-string type2str(int type) 
-{
-	string r;
-
-	uchar profondeur = type & CV_MAT_DEPTH_MASK;
-	uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-	switch(profondeur) 
-	{
-		case CV_8U:  r = "8U"; break;
-		case CV_8S:  r = "8S"; break;
-		case CV_16U: r = "16U"; break;
-		case CV_16S: r = "16S"; break;
-		case CV_32S: r = "32S"; break;
-		case CV_32F: r = "32F"; break;
-		case CV_64F: r = "64F"; break;
-		default:     r = "User"; break;
-	}
-
-	r += "C";
-	r += (chans+'0');
-
-	return r;
-}
+#include "outils/outils.hpp"
 
 struct TroisCanaux
 {
@@ -39,29 +7,7 @@ struct TroisCanaux
 	Mat canal3;	
 };
 
-Mat charge_image(int argc, char** argv)
-{
-	// On charge l'image donnée en paramètre (par défaut img.png dans le dossier source) en noir et blanc
-	CommandLineParser parser(argc, argv, "{@image | ../../../../images/toy_example.jpg | Image sur laquelle on va opérer nos transformations}");
-	Mat image_chargee = imread(parser.get<String>( "@image" ), IMREAD_GRAYSCALE);
-	
-	// On floute un peu l'image pour réduire le bruit
-	//GaussianBlur(image_source, image_source, Size(5, 5), 0, 0, BORDER_DEFAULT);
-	Mat image_source;
-	bilateralFilter(image_chargee, image_source, 10, 20, 5);
-	return image_source;
-}
 
-Mat charge_image_hsv(int argc, char** argv)
-{
-	// On charge l'image donnée en paramètre du programme dans l'espace de couleur BGR
-	CommandLineParser parser(argc, argv, "{@image | ../../../../images/toy_example.jpg | Image sur laquelle on va opérer nos transformations}");
-	Mat image_chargee = imread(parser.get<String>( "@image" ), IMREAD_COLOR);
-	// On convertit l'image dans l'espace de couleur HSV
-	Mat image_source;
-	cvtColor(image_chargee, image_source, COLOR_BGR2HSV);
-	return image_source; 
-}
 
 // Coder fonction pour le calcul de Texton Color Space (TCS)
 // Définir un nouveau type struct pour renvoyer les 3 canaux
---
+++ src/outils/outils.hpp
@@ -0,0 +1,102 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+string type2str(int type) 
+{
+	string r;
+	uchar profondeur = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
+	switch(profondeur) 
+	{
+		case CV_8U:  r = "8U"; break;
+		case CV_8S:  r = "8S"; break;
+		case CV_16U: r = "16U"; break;
+		case CV_16S: r = "16S"; break;
+		case CV_32S: r = "32S"; break;
+		case CV_32F: r = "32F"; break;
+		case CV_64F: r = "64F"; break;
+		default:     r = "User"; break;
+	}
+	r += "C";
+	r += (chans+'0');
+	return r;
+}
+
+vector<Mat> charge_repertoire_images(string repertoire, string extension)
+{
+	// Charge toutes les images dans `repertoire` avec l'extension `extension`
+	// `repertoire` ne doit pas comporter de "/" à la fin.
+	// `extension` ne doit pas comporter de "." au début
+	vector<cv::String> emplacements;
+	cout << "Recherche d'images dans " << repertoire + "/*." + extension << endl;
+	glob(repertoire + "/*." + extension, emplacements, false);
+
+	vector<Mat> images;
+	size_t nb_images = emplacements.size(); 
+	Mat image_courante;
+
+	for(size_t i = 0; i < nb_images; ++i)
+	{
+		cout << "Lecture de " + emplacements[i] << endl;
+		image_courante = imread(emplacements[i], IMREAD_GRAYSCALE);
+		Mat img_filtree;
+		bilateralFilter(image_courante, img_filtree, 10, 20, 5);
+    	images.push_back(img_filtree);
+	}
+	
+	return images;
+}
+
+Mat charge_image(int argc, char** argv)
+{
+	// On charge l'image donnée en paramètre (par défaut img.png dans le dossier source) en noir et blanc
+	CommandLineParser parser(argc, argv, "{@image | ../../../../images/toy_example.jpg | Image sur laquelle on va opérer nos transformations}");
+	Mat image_chargee = imread(parser.get<String>( "@image" ), IMREAD_GRAYSCALE);
+	// On floute un peu l'image pour réduire le bruit
+	//GaussianBlur(image_source, image_source, Size(5, 5), 0, 0, BORDER_DEFAULT);
+	Mat image_source;
+	bilateralFilter(image_chargee, image_source, 10, 20, 5);
+	return image_source;
+}
+
+Mat charge_image_hsv(int argc, char** argv)
+{
+	// On charge l'image donnée en paramètre du programme dans l'espace de couleur BGR
+	CommandLineParser parser(argc, argv, "{@image | ../../../../images/toy_example.jpg | Image sur laquelle on va opérer nos transformations}");
+	Mat image_chargee = imread(parser.get<String>( "@image" ), IMREAD_COLOR);
+	// On convertit l'image dans l'espace de couleur HSV
+	Mat image_source;
+	cvtColor(image_chargee, image_source, COLOR_BGR2HSV);
+	return image_source; 
+}
+
+Mat cree_image_orientations(Mat orientations)
+{
+	// Pour afficher des orientations uniquement, avec 0 = absence d'orientation.
+	// Les orientations sont comprises entre 0 et 180, et doivent donc être stockées sur 8 bits
+	Mat intensites = Mat(orientations.size(), CV_8UC1);
+	Mat saturations = Mat(orientations.size(), CV_8UC1);
+	intensites = 1;
+	saturations = 1;
+	for(int x = 0; x < orientations.size().width; ++x)
+	{
+		for(int y = 0; y < orientations.size().height; ++y)
+		{
+			if(orientations.at<uint8_t>(x,y) == 0) intensites<uint8_t>(x,y) = 0;
+		}
+	}
+	vector<Mat> canaux;
+	canaux.push_back(orientations);
+	canaux.push_back(saturations);
+	canaux.push_back(intensites);
+	Mat res;
+	merge(canaux, res);
+	cvtColor(res, res, COLOR_HSV2BGR);
+	return res;
+}
