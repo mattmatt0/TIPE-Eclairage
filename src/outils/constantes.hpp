a604a1b47c30d642b286142036c35a88b2005f44
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-14 16:46:56 +0200

    Suppression des types inutiles

---
+++ src/outils/constantes.hpp
@@ -0,0 +1,6 @@
+string const params_generiques = "{help h || Affiche ce message}";
+string const params_analyse = "{nb-seuils | 10 | Nombre de seuils à étudier}{nb-orientations | 10 | Nombre d'orientations possibles}{tau | 2 | Temps de stabilité (valeur de T)}";
+string const params_images = "{rep-source | ../../../../images/test_mfs | Dossier contenant la séquence à analyser}{extension | png | Extension des images à rechercher}{nb-img | -1 | Nombre d'images à charger}";
+
+unsigned char TOUCHE_N = 110;
+unsigned char TOUCHE_Q = 113;
