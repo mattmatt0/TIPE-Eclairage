2024ec3161460c8663faa226d8e344e47cde97b5
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-14 07:47:36 +0200

    Amélioration de l'affichage

--- tests/generation_mfs/src/main.cpp
+++ tests/generation_mfs/src/main.cpp
@@ -2,24 +2,27 @@
 
 int main(int argc, char** argv)
 {	
-	CommandLineParser parser(argc, argv, "{@source_dir | ../../../../images/test_mfs | Dossier contenant la séquence à analyser}{@extension | png | Extension des images à rechercher}");
-	vector<Mat> images = charge_repertoire_images(parser.get<String>("@source_dir"), parser.get<String>("@extension"));
+	CommandLineParser parser(argc, argv, "{source_dir | ../../../../images/test_mfs | Dossier contenant la séquence à analyser}{extension | png | Extension des images à rechercher}{nb_images | -1 | Nombre d'images à étudier}");
+	vector<Mat> images = charge_repertoire_images(parser.get<String>("source_dir"), parser.get<String>("extension"));
 	vector<Mat> ensembles_O;
 	int const NB_SEUILS = 10;
 	int const NB_ORIENTATIONS = 10;
 	int const T = 2;
-	for(int i = 0; i < images.size(); ++i)
+	int nb_images = parser.get<int>("nb_images");
+	cout << "Nombre d'images: " << nb_images << endl;
+	nb_images = (nb_images == -1) ? images.size() : nb_images;
+	for(int i = 0; i < nb_images; ++i)
 	{
-		cout << "Traitement de l'image n° " << i+1 << "/" << images.size() << endl;
+		cout << "Traitement de l'image n° " << i+1 << "/" << nb_images << endl;
 		vector<Mat> ensembles_X = separe_en_seuils(images.at(i), NB_SEUILS);
 		vector<Mat> contours = calcul_contours(images.at(i), ensembles_X, NB_SEUILS);
 		vector<Mat> S_et_O = calcul_S_et_O(contours, NB_SEUILS);
 		ensembles_O.push_back(S_et_O.at(0));
 	}
+	int n;
 	for(int t = 0; t + T < ensembles_O.size(); ++t)
 	{
 
-		cout << "Calcul de D entre t = " << t << " et t = " << t+T << endl;
 		Mat D = calcule_D(ensembles_O, t, T, NB_ORIENTATIONS);
 		Mat disp_img = cree_image_orientations(D);
 		imshow("D", disp_img);
@@ -27,7 +30,11 @@ int main(int argc, char** argv)
 		ensembles_O.at(t+T).convertTo(O_entier, CV_8UC1);
 		Mat disp_imgO = cree_image_orientations(O_entier);
 		imshow("O", disp_imgO);
-		while(waitKeyEx() != 113);
+		while(n = waitKeyEx())
+		{
+			if(n == 113) {t = ensembles_O.size(); break;}
+			else if (n == 110) break;
+		};
 	}
 	return 0;
 }
