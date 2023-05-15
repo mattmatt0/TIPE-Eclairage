988caede7f6a377095193c145bc1bfe9941418c2
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-15 16:53:42 +0200

    Correction des bogues

--- src/detection_contours/detection_contours.hpp
+++ src/detection_contours/detection_contours.hpp
@@ -30,46 +30,37 @@ void _calcule_tables_tcs()
 }
 
 // Tables pour l'orientation et la norme
-array<uint8_t, 37*37> table_orientations;
+array<uint8_t, 97*97> table_orientations;
 
-void _calcule_orientations(uint8_t nb_seuils)
+void _calcule_orientations(uint8_t nb_orientations)
 {
 	uint8_t orientation;
-	for(int dx = -18; dx <= 18; ++dx)
+	for(int dx = -48; dx <= 48; ++dx)
 	{
-		for(int dy = -18; dy <= 18; ++dy)
+		for(int dy = -48; dy <= 48; ++dy)
 		{
 			if(dx == 0 && dy == 0)
 				orientation = 0;
 			else
-				orientation = floor((atan2(dy,dx)/(2*pi) + 0.5) * nb_seuils);
-			table_orientations.at((dx+18)*37 + (dy*18)) = orientation;
+				orientation = floor((atan2(dy,dx)/(2*PI) + 0.5) * nb_orientations);
+			table_orientations.at((dx+48)*49 + dy+48) = orientation;
 		}
 	}
 }
 
-array<uint8_t, 37*37> table_normes;
-void _calcule_normes(uint8_t nb_seuils)
+array<uint8_t, 97*97> table_normes;
+void _calcule_normes()
 {
 	uint8_t val;
-	for(int dx = -18; dx <= 18; ++dx)
+	for(int dx = -48; dx <= 48; ++dx)
 	{
-		for(int dy = -18; dy <= 18; ++dy)
+		for(int dy = -48; dy <= 48; ++dy)
 		{
-			val = (dx*dx + dy*dy) > 2 ? 1 : 0;
-			table_normes.at((dx+18)*37 + (dy*18)) = val;
+			val = (dx*dx + dy*dy) > 0 ? 1 : 0;
+			table_normes.at((dx+48)*97 + dy+48) = val;
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -115,21 +106,20 @@ array<Mat, 3> calcul_TCS (array<Mat, 3> tab_mat)
 
 
 
-
 template <int n> array<array<Mat, n>, 2> calcul_contours(array<Mat, n>& ensembles_X)
 {
 	// Calcul des contours
 	array<Mat, n> contours;
-	array<Mat, n> orientation;
-	Size taille = ensemblesX.at(0).size();
+	array<Mat, n> orientations;
+	Size taille = ensembles_X.at(0).size();
 	uint16_t tailleX = taille.width;
 	uint16_t tailleY = taille.height;
 	for(int i = 0; i < n; ++i)
 	{
 		// Calcul des dérivées
 		Mat gradient_x, gradient_y;
-		Sobel(ensembles_X.at(i), gradient_x, CV_8C1, 1, 0, 5);
-		Sobel(ensembles_X.at(i), gradient_y, CV_8C1, 0, 1, 5);
+		Sobel(ensembles_X.at(i), gradient_x, CV_16SC1, 1, 0, 5);
+		Sobel(ensembles_X.at(i), gradient_y, CV_16SC1, 0, 1, 5);
 
 		Mat orientation = Mat(taille, CV_8UC1);
 		Mat contour = Mat(taille, CV_8UC1);
@@ -139,7 +129,7 @@ template <int n> array<array<Mat, n>, 2> calcul_contours(array<Mat, n>& ensemble
 		{
 			for(int y = 0; y < tailleY; ++y)
 			{
-				pos = (gradient_x.at<int8_t>(y,x)+18)*37 + gradient_y.at<int8_t>(y,x)+18;
+				pos = (gradient_x.at<int16_t>(y,x)+48)*97 + gradient_y.at<int16_t>(y,x)+48;
 				orientation.at<uint8_t>(y,x) = table_orientations.at(pos);
 				contour.at<uint8_t>(y,x) = table_normes.at(pos);
 			}
@@ -154,7 +144,7 @@ template <int n> array<array<Mat, n>, 2> calcul_contours(array<Mat, n>& ensemble
 
 
 
-template<int nb_seuils> array<Mat, 2> calcul_S_et_O(array<Mat, nb_seuils> appartient_contours, array<Mat, nb_seuils> orientations, int nb_orientations)
+template<int nb_seuils> array<Mat, 2> calcul_S_et_O(array<Mat, nb_seuils> contours, array<Mat, nb_seuils> orientations, int nb_orientations)
 {
 	Size taille = contours.at(0).size();
 	int taille_x = taille.height;
@@ -162,34 +152,39 @@ template<int nb_seuils> array<Mat, 2> calcul_S_et_O(array<Mat, nb_seuils> appart
 	// Calcul de S et O
 	Mat ensemble_S = Mat::zeros(taille, CV_8UC1);
 	Mat ensemble_O = Mat::zeros(taille, CV_8UC1);
+	uint16_t *nb_occurrences = (uint16_t*) malloc(sizeof(uint16_t)*nb_orientations);
+
 	for(int x = 0; x < taille_x; ++x)
 	{
 		for(int y = 0; y < taille_y; ++y)
 		{
 			// On calcule l'orientation majoritaire
-			array<uint16_t, nb_orientations> nb_occurences;
-			nb_occurences.fill(0);
+			for(int i = 0; i < nb_orientations; ++i) nb_occurrences[i] = 0;
 			// On compte, pour chaque orientation, le nombre de contours pour lequel il y a cette orientation
 			for(int i = 0; i < nb_seuils; ++i)
 			{
-				if(appartient_contours.at(i).template at<uint8_t>(x,y) == 1)
-					nb_occurences[orientations.at(i).template at<uint8_t>(x,y)]++;
+				if(contours.at(i).template at<uint8_t>(x,y) == 1)
+				{
+					ensemble_S.at<uint8_t>(x,y)++;
+					nb_occurrences[orientations.at(i).template at<uint8_t>(x,y)]++;
+				}
 			}
 			int orientation_majoritaire = 0;
 			int nb_orientation_max = 0;
 			for(int i = 0; i < nb_orientations; ++i)
 			{
-				if(nb_occurences.at(i) > nb_orientation_max)
+				if(nb_occurrences[i] > nb_orientation_max)
 				{
 					orientation_majoritaire = i;
-					nb_orientation_max = nb_occurences.at(i);
+					nb_orientation_max = nb_occurrences[i];
 				}
 			}
 			// On l'ajoute dans l'ensemble O
 			ensemble_O.at<uint8_t>(x,y) = orientation_majoritaire;
 		}
 	}
-	array<Mat, 2> canaux = {ensemble_O, ensemble_S};
+	free(nb_occurrences);
+	array<Mat, 2> canaux = {ensemble_S, ensemble_O};
 	return canaux;
 }
 
@@ -224,14 +219,14 @@ array<Mat, 2> synthese_S_O_CTS(array<Mat, 3> ensembles_S, array<Mat, 3> ensemble
 	return res;
 }
 
-array<Mat, 2> calcule_OS_NB(Mat source, int nb_seuils)
+template <int nb_seuils> array<Mat, 2> calcule_OS_NB(Mat source, int nb_orientations)
 {
 	array<Mat, nb_seuils> ensembles_X = separe_en_seuils<nb_seuils>(source);
 	array<array<Mat, nb_seuils>, 2> contours = calcul_contours<nb_seuils>(ensembles_X);
-	return calcul_S_et_O(contours.at(0), contours.at(1));
+	return calcul_S_et_O<nb_seuils>(contours.at(0), contours.at(1), nb_orientations);
 }
 
-array<Mat, 2> calcule_OS_CTS(Mat source, int nb_seuils)
+template <int nb_seuils> array<Mat, 2> calcule_OS_CTS(Mat source, int nb_orientations)
 {
 	array<Mat, 3> canaux = separe_hsv(source);
 	array<Mat, 3> ensembles_O;
@@ -239,7 +234,7 @@ array<Mat, 2> calcule_OS_CTS(Mat source, int nb_seuils)
 	array<Mat, 2> ensemble_SO;
 	for(int i = 0; i < 3; ++i)
 	{
-		ensemble_SO = calcule_OS_NB(canaux.at(i), nb_seuils);
+		ensemble_SO = calcule_OS_NB<nb_seuils>(canaux.at(i), nb_orientations);
 		ensembles_S.at(i) = ensemble_SO.at(0);
 		ensembles_O.at(i) = ensemble_SO.at(1);
 	}
--- tests/detection_contours/src/main.cpp
+++ tests/detection_contours/src/main.cpp
@@ -10,58 +10,18 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	Mat image_source = charge_image_hsv(argc, argv);
-	array<Mat, 3> image_source_splited = separe_hsv(image_source);
-	array<Mat, 3> TCS_img = calcul_TCS(image_source_splited);	
-	imshow("img1", TCS_img.at(0));
-	
+	Mat image_source = charge_image(argc, argv);
 
 	// Nombre de seuils pour les lignes de niveaux
 	int const NB_SEUILS = 10;
+	int const NB_ORIENTATIONS = 20;
 
-	// Séparation des pixels répartis dans les NB_SEUILS seuils.
-	array<Mat, NB_SEUILS> ensembles_X1 = separe_en_seuils<NB_SEUILS>(TCS_img.at(0));
-	array<Mat, NB_SEUILS> ensembles_X2 = separe_en_seuils<NB_SEUILS>(TCS_img.at(1));
-	array<Mat, NB_SEUILS> ensembles_X3 = separe_en_seuils<NB_SEUILS>(TCS_img.at(2)); 
 
-	// Calcul des contours
-	array<Mat, NB_SEUILS> contours1 = calcul_contours<NB_SEUILS>(ensembles_X1);
-	array<Mat, NB_SEUILS> contours2 = calcul_contours<NB_SEUILS>(ensembles_X2);
-	array<Mat, NB_SEUILS> contours3 = calcul_contours<NB_SEUILS>(ensembles_X3);
+	_calcule_orientations(NB_ORIENTATIONS);
+	_calcule_normes();
+	array<Mat, 2> OS = calcule_OS_NB<NB_SEUILS>(image_source, NB_ORIENTATIONS);
+	imshow("", cree_image_orientations(MFS));
 
-	// Calcul des canaux hsv
-	array<Mat, 2> canaux1 = calcul_S_et_O<NB_SEUILS>(contours1);
-	array<Mat, 2> canaux2 = calcul_S_et_O<NB_SEUILS>(contours2);
-	array<Mat, 2> canaux3 = calcul_S_et_O<NB_SEUILS>(contours3);
-	Mat ensemble_O1 = canaux1.at(0);
-	Mat ensemble_S1 = canaux1.at(1);
-	Mat ensemble_O2 = canaux2.at(0);
-	Mat ensemble_S2 = canaux2.at(1);
-	Mat ensemble_O3 = canaux3.at(0);
-	Mat ensemble_S3 = canaux3.at(1);
-
-	array<Mat, 2> synth1;
-	array<Mat, 2> synth2;
-	array<Mat, 2> synth3;
-
-	synth1 = {ensemble_O1 ,ensemble_S1};
-	synth2 = {ensemble_O2 ,ensemble_S2};
-	synth3 = {ensemble_O3 ,ensemble_S3};
-
-	array<array<Mat, 2>, 3> datas = {synth1, synth2, synth3};
-
-	
-	
-	// Calcul de l'image de synthèse
-	array<Mat, 3> mfs_cts = synthese_S_O_CTS(datas);
-
-	Mat img_mfs_cts;
-	merge(mfs_cts, img_mfs_cts);
-	cvtColor(img_mfs_cts, img_mfs_cts, COLOR_HSV2BGR);
-
-	imshow("MFS CTS", img_mfs_cts);
-	
-	// On attend que la touche `q` ait été pressée
 	while(waitKeyEx() != 113);
 	return 0;
 }
