46de62a7c802ab925513160077874b51f3f1657d
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-16 10:59:41 +0200

    Suppression du seuil de tolérance

--- src/generation_mfs/generation_mfs.hpp
+++ src/generation_mfs/generation_mfs.hpp
@@ -5,20 +5,10 @@ Mat calcule_R(vector<Mat> ensembles_O, int t, int T, int nb_orientations)
 	// Calcule R_t à partir du vecteur des valeurs de O à chaque instant t.
 	// R_t est l'ensemble des points ayant une orientation fixe pendant T images.
 	// L'image renvoyée contient des entiers valant 0 si le pixel n'est pas dans R, et un entier représentant l'orientation du pixel sinon.
-	int seuil = 360 / (nb_orientations+1);
 	int taille_x = ensembles_O.at(0).size().height;
 	int taille_y = ensembles_O.at(0).size().width;
-	// Création de la première image à laquelle on va comparer les images suivantes.
-	// On va arrondir les valeurs des orientations comme il faut, dans un demi-tour
-	Mat res = Mat(ensembles_O.at(0).size(), CV_8UC1);
-	cout << "Conversion de l'image en valeurs entières..." << endl;
-	for(int x = 0; x < taille_x; ++x)
-	{
-		for(int y = 0; y < taille_y; ++y)
-		{
-			res.at<uint8_t>(x,y) = floor(floor(ensembles_O.at(t).at<uint8_t>(x,y) / 360 * nb_orientations) * seuil);
-		}
-	}
+
+	Mat res = ensembles_O.at(0).clone();
 	
 	cout << "Vérification de la stabilité..." << endl;
 	// Puis on vérifie la stabilité des orientations
@@ -28,8 +18,7 @@ Mat calcule_R(vector<Mat> ensembles_O, int t, int T, int nb_orientations)
 		{
 			for(int i = t+1; i < t+T; ++i)
 			{
-				if(res.at<uint8_t>(x,y) > ensembles_O.at(i).at<uint8_t>(x,y) + seuil ||
-					res.at<int8_t>(x,y) < ensembles_O.at(i).at<uint8_t>(x,y) - seuil)
+				if(res.at<uint8_t>(x,y) != ensembles_O.at(i).at<uint8_t>(x,y))
 				{
 					res.at<uint8_t>(x,y) = 255;
 					break;
@@ -57,9 +46,9 @@ Mat calcule_D(vector<Mat> images, int t, int T, int nb_orientations)
 	{
 		for(int y = 0; y < taille_y; ++y)
 		{
-			if(R.at<uint8_t>(x,y) == 255 || R.at<uint8_t>(x,y) > images.at(t+T).at<uint8_t>(x,y) + seuil || R.at<uint8_t>(x,y) < images.at(t+T).at<uint8_t>(x,y) - seuil)
+			if(R.at<uint8_t>(x,y) == 255 || R.at<uint8_t>(x,y) != images.at(t+T).at<uint8_t>(x,y))
 			{
-				res.at<uint8_t>(x,y) = floor(images.at(t+T).at<uint8_t>(x,y));
+				res.at<uint8_t>(x,y) = images.at(t+T).at<uint8_t>(x,y);
 			}
 		}
 	}
