142398665384ef9d905ae35625f113511de5694c
Matthieu XILLO <matthieu.xillo@hotmail.com>
2023-05-18 12:18:12 +0200

    Adaptation du chemin par défaut pour les dossiers d'images à l'arborescence imposée par le script de conversion

--- src/outils/constantes.hpp
+++ src/outils/constantes.hpp
@@ -2,8 +2,8 @@
 string const params_generiques = "{help h || Affiche ce message}";
 string const params_image = "{img-src | ../../../../images/lenna.jpg | Image sur laquelle on va calculer effectuer nos opérations}";
 string const params_analyse = "{nb-seuils | 10 | Nombre de seuils à étudier}{nb-orientations | 10 | Nombre d'orientations possibles}";
-string const params_images = "{rep-src | ../../../../images/test_ecm | Dossier contenant la séquence à analyser}{extension | png | Extension des images à rechercher}{nb-img | -1 | Nombre d'images à charger}{tau | 2 | Temps de stabilité (valeur de T)}";
-string const param_sauvegarde_dossier = "{rep-dest | ../../../../images/resultats | Dossier contenant les résultats}";
+string const params_images = "{rep-src | ../../../../images/sources/video1 | Dossier contenant la séquence à analyser}{extension | png | Extension des images à rechercher}{nb-img | -1 | Nombre d'images à charger}{tau | 2 | Temps de stabilité (valeur de T)}";
+string const param_sauvegarde_dossier = "{rep-dest | ../../../../images/resultats/video1 | Dossier contenant les résultats}";
 
 
 unsigned char TOUCHE_N = 110;
